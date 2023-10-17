a784d3b41545b7c0b57054b2426475afe846fd9b
Nick <nick@ncofer.com>
2023-10-17 11:49:33 -0400

    finished singlyLinkedList class

--- code_template/SinglyLinkedList.java
+++ code_template/SinglyLinkedList.java
@@ -8,8 +8,24 @@ import java.util.Iterator;
 */
 public class SinglyLinkedList<T extends Comparable<T>> implements Iterable<T>
 {
+
+    private Node head;
+    private int size;
+
+    private class Node {
+        T value;
+        Node next;
+
+        Node(T value){
+            this.value = value;
+        }
+    }
+
+
     public SinglyLinkedList()
     {
+        this.head = null;
+        this.size = 0;
     }
 
     /**
@@ -19,6 +35,17 @@ public class SinglyLinkedList<T extends Comparable<T>> implements Iterable<T>
     */
     public void add(T value)
     {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        size++;
     }
 
     /**
@@ -28,6 +55,19 @@ public class SinglyLinkedList<T extends Comparable<T>> implements Iterable<T>
     */
     public void insert(T newValue)
     {
+        Node newNode = new Node(newValue);
+        if (head == null || head.value.compareTo(newValue) <= 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null && temp.next.value.compareTo(newValue) > 0) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+        size++;
     }
 
     /**
@@ -37,6 +77,25 @@ public class SinglyLinkedList<T extends Comparable<T>> implements Iterable<T>
     */
     public T remove(int index)
     {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node temp = head;
+        if (index == 0) {
+            head = head.next;
+            size--;
+            return temp.value;
+        }
+
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        Node nodeToRemove = temp.next;
+        temp.next = temp.next.next;
+        size--;
+        return nodeToRemove.value;
     }
 
     /**
@@ -46,6 +105,14 @@ public class SinglyLinkedList<T extends Comparable<T>> implements Iterable<T>
     */
     public T get(int index)
     {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.value;
     }
 
     /**
@@ -53,6 +120,7 @@ public class SinglyLinkedList<T extends Comparable<T>> implements Iterable<T>
     */
     public int size()
     {
+        return this.size;
     }
 
     /**
@@ -60,11 +128,25 @@ public class SinglyLinkedList<T extends Comparable<T>> implements Iterable<T>
     */
     public boolean isEmpty()
     {
+        return this.size == 0;
     }
 
     @Override
     public Iterator<T> iterator() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+        return new Iterator<T>() {
+            private Node current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T value = current.value;
+                current = current.next;
+                return value;
+            }
+        };
     }
 }
