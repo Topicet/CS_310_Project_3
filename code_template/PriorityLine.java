2deee0bbb305ed52a9c40e8849f8560ca8684365
Nick <nick@ncofer.com>
2023-10-19 17:49:44 -0400

    finished priorityLine for now

--- code_template/PriorityLine.java
+++ code_template/PriorityLine.java
@@ -1,48 +1,96 @@
+import java.util.Iterator;
+
 /**
-    - Implements a priority queue
-    - You must implement all the public methods in this template plus the method(s) required by the Iterable interface
-    - Anything else you add must be private
-    - Do not modify the provided signatures
-*/
+ * Implements a priority queue.
+ * You must implement all the public methods in this template plus the method(s) required by the Iterable interface.
+ * Anything else you add must be private.
+ * Do not modify the provided signatures.
+ *
+ * @param <T> The type of elements stored in the priority queue, must implement Comparable.
+ */
 public class PriorityLine<T extends Comparable<T>> implements Iterable<T>
 {
+
+    SinglyLinkedList<T> priorityQueue;
+
+
+    /**
+     * Initializes an empty priority queue.
+     */
     public PriorityLine()
     {
+        this.priorityQueue = new SinglyLinkedList<T>();
     }
 
     /**
-        TIME COMPLEXITY REQUIREMENT: O(N)
-    */
+     * Inserts an element into the priority queue.
+     *
+     * @param element The element to be inserted.
+     * @implNote This operation has a time complexity of O(N).
+     */
     public void enqueue(T element)
     {
+        if(element!=null)   this.priorityQueue.insert(element);
     }
 
+
     /**
-        TIME COMPLEXITY REQUIREMENT: O(1)
-    */
+     * Removes and returns the highest-priority element from the priority queue.
+     *
+     * @return The highest-priority element, or null if the priority queue is empty.
+     * @implNote This operation has a time complexity of O(1).
+     */
     public T dequeue()
     {
+        if(this.priorityQueue.isEmpty()) return null;
+
+        return this.priorityQueue.remove(0);
     }
 
     /**
-        TIME COMPLEXITY REQUIREMENT: O(1)
-    */
+     * Returns the number of elements in the priority queue.
+     *
+     * @return The number of elements.
+     * @implNote This operation has a time complexity of O(1).
+     */
     public int size()
     {
+        return this.priorityQueue.size();
     }
 
     /**
-        TIME COMPLEXITY REQUIREMENT: O(1)
-    */
+     * Checks if the priority queue is empty.
+     *
+     * @return True if the priority queue is empty, false otherwise.
+     * @implNote This operation has a time complexity of O(1).
+     */
     public boolean isEmpty()
     {
+        return this.priorityQueue.size() == 0;
     }
 
     /**
-        TIME COMPLEXITY REQUIREMENT: O(1)
-    */
+     * Returns the highest-priority element in the priority queue without removing it.
+     *
+     * @return The highest-priority element, or null if the priority queue is empty.
+     * @implNote This operation has a time complexity of O(1).
+     */
     public T peek()
     {
+        if (this.priorityQueue.isEmpty()) {
+            return null;  // or throw an exception, depending on your needs
+        }
+        return this.priorityQueue.get(0);
+    }
+
+    /**
+     * Returns an iterator over the elements in the priority queue.
+     *
+     * @return An iterator over the elements.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return this.priorityQueue.iterator();
     }
 
 }
