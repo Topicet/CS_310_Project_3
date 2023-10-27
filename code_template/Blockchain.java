a853dd12b44a6eeb9080566717540da1b777f5b4
Nick <nick@ncofer.com>
2023-10-27 14:14:38 -0400

    Blockchain done

--- code_template/Blockchain.java
+++ code_template/Blockchain.java
@@ -1,23 +1,28 @@
 import java.util.Iterator;
 
 /**
-    - The class represents the entire blockchain
-    - You must implement all the public methods in this template plus the method(s) required by the Iterable interface
-    - Anything else you add must be private
-    - Do not modify the provided signatures
+    - The class represents the entire blockchain.
+    - You must implement all the public methods in this template plus the method(s) required by the Iterable interface.
+    - Anything else you add must be private.
+    - Do not modify the provided signatures.
 */
 
 public class Blockchain implements Iterable<Block>
 {
 
+    /**
+     * The linked list of blocks that constitute the block chain itself.
+     */
     private SinglyLinkedList<Block> blockchainList;
 
     /**
-        The concstructor takes a priority queue and creates the linked list of blocks
+        The concstructor takes a priority queue and creates the linked list of blocks.
 
         @param threshold is the minimum amount of cumulative fees that is required to create a new block.
         The block must contain the minimum number of transactions that satisfy the threshold criterion
 
+        @param queue The list of transactions that make up each block that will be added into the block chain.
+        
         TIME COMPLEXITY REQUIREMENT: O(N)
     */
     public Blockchain(PriorityLine<Transaction> queue, int threshold)
